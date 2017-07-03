de641d520207240e80ed203683fb780fa143331f
ssai123 <sbs.sai@gmail.com>
2017-07-02 23:26:06 -0400

    charts and nav bar fixes

--- animation.js
+++ animation.js
@@ -25,15 +25,42 @@ $(document).ready(function() {
   )
 });
 
-// hover for vertical dot nav bar
+
+
+// hover and transition effects for vertical dot nav bar
 $(document).ready(function($){
+        var parPosition = [];
+      $('.container-fluid').each(function() {
+          parPosition.push($(this).offset().top);
+      });
+        $('.Navbar ul li a').click(function () {
+    $('.Navbar ul li a').removeClass('active');
+      $(this).addClass('active');
+  });
      $('.Navbar a').hover(function() {
          $(this).find('.label').show();
          }, function() {
          $(this).find('.label').hide();
      });
+         $(document).scroll(function(){
+      var position = $(document).scrollTop(),
+              index;
+              for (var i=0; i<parPosition.length; i++) {
+              if (position <= parPosition[i] * 1.5) {
+                  index = i;
+                  break;
+              }
+          }
+    $('.Navbar ul li a').removeClass('active');
+          $('.Navbar ul li a:eq('+index+')').addClass('active');
+      });
+        $('.Navbar ul li a').click(function () {
+    $('.Navbar ul li a').removeClass('active');
+      $(this).addClass('active');
+  });
 });
 
+// Handles fading in and out of navbar
 $(document).ready(function() {
   $(".Navbar").hide(); //Hide the navigation bar first
   $(document).on('scroll', function() {
@@ -45,6 +72,28 @@ $(document).ready(function() {
   });
 });
 
+
+var inView = false;
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+}
+
+$(window).scroll(function() {
+    if (isScrolledIntoView('#myChart')) {
+        if (inView) { return; }
+        inView = true;
+        new Chart(document.getElementById("myChart").getContext("2d")).Pie(data);
+    } else {
+        inView = false;
+    }
+});
 
 
 
